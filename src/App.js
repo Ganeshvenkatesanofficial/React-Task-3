bbd8377e8850a71f36002d2e85e3d8f5115cd46d
GANESH V <sit22co045@sairamtap.edu.in>
2023-12-17 08:51:22 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import TodoList from './todo_app/TodoList';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+function App() {
+  return (
+    <div className="App" style={{ fontFamily: 'Rubik Bubbles, sans-serif' }}>
+     <TodoList/>
+    </div>
+  );
+}
+
+export default App;
